7186fd9a74086ccac636b5bdf3be0316182e7a28
RemondJu <remond.neiluj@gmail.com>
2018-10-22 15:41:20 +0200

    Quest done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import FormEmployee from './components/FormEmployee';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <FormEmployee />
+      </div>
+    );
+  }
+}
+
+export default App;
